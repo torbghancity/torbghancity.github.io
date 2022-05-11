55bf4478325a4c00e72ba0c0576612b19ac8efb0
sobhan <99309178+salari12@users.noreply.github.com>
2022-05-11 18:30:20 +0430

    Update java.js

--- torshiz/javascript/java.js
+++ torshiz/javascript/java.js
@@ -1,8 +1,11 @@
 function size() {
     document.getElementById("s1").setAttribute("class", "democlass");
     console.log("size");
+    document.getElementById("s1").style.color="blue";
 }
 function font(){
     document.getElementById("s2").setAttribute("class","demclass");
     console.log("font");
+    document.getElementById("s2").style.backgroundColor="red";
+    document.getElementById("s2").style.marginTop="150px";
 }
