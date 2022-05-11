7f170c64448e2a78e518d4536e9e48f49ec811ab
sobhan <99309178+salari12@users.noreply.github.com>
2022-05-11 22:33:19 +0430

    update

--- torshiz/javascript/java.js
+++ torshiz/javascript/java.js
@@ -9,3 +9,9 @@ function font(){
     document.getElementById("s2").style.backgroundColor="red";
     document.getElementById("s2").style.marginTop="150px";
 }
+function carw(){
+    document.getElementById("s3").setAttribute("dess","demss");
+    document.getElementById("s3").style.fontSize="25px";
+    console.log("carw");
+    document.getElementById("s3").style.display="none";
+}
